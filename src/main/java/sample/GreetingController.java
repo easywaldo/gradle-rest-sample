3169f7023a5c9ea722e63faa5bda33d4ba638ab6
easywaldoji <easywaldoji@gmail.com>
2019-10-28 23:35:02 +0900

    Greeting Add / Search Action 추가

--- src/main/java/sample/GreetingController.java
+++ src/main/java/sample/GreetingController.java
@@ -1,21 +1,45 @@
 package sample;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    protected static List<Greeting> greetingList = new ArrayList<>();
 
+    @GetMapping
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(
             value="name",
             defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+        Greeting result = new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+        return result;
+    }
+
+    @GetMapping
+    @RequestMapping("/add")
+    public void Add(
+            @RequestParam(value="id") Long id,
+            @RequestParam(value="name") String name) {
+        greetingList.add(
+                new Greeting(id, String.format(template, name)));
+    }
+
+    @GetMapping
+    @RequestMapping("/search")
+    public Greeting Search(@RequestParam(
+            value="id") long id) {
+
+        return greetingList.stream()
+                .filter(x -> x.getId() == id)
+                .findFirst().get();
+
     }
 }
